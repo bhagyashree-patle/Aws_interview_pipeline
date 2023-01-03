7c4c839723b4508dd7ae83f7ab10a0a8f7d473c3
gayatri-atgeir <121789912+gayatri-atgeir@users.noreply.github.com>
2023-01-03 18:22:57 +0530

    Update master_table.py
    Code changed for city, state and pincode. A single consolidated function for all three

--- create_data/master_table.py
+++ create_data/master_table.py
@@ -1,7 +1,7 @@
 import random
 from datetime import datetime
 from datetime import date
-
+import csv
 
 class BankDetails:
 
@@ -21,16 +21,12 @@ class BankDetails:
         create_date = datetime(year, month, day)
         return create_date
 
-    def getCity(self):
-        city_list = ["Pune", "Mumbai", "Bangalore", "Calcutta", "Delhi", "Jaipur", "Ahmedabad", "Rajkot"]
-        city_name = random.choice(city_list)
-        return city_name
-
-    def getState(self, city_name):
-        # print(city_name)
-        state_dict = {"Pune": "Maharashtra", "Mumbai": "Maharashtra", "Bangalore": "Karnataka", "Calcutta": "West Bengal",
-                      "Delhi": "Delhi", "Jaipur": "Rajasthan", "Ahmedabad": "Gujarat", "Rajkot":"Gujarat"}
-        return state_dict.get(city_name)
+    def getAdressdetails(self):
+        with open('C:/Users/HP/Practice Assignments/pincode_details.csv') as file:
+            reader = csv.reader(file)
+            chosen_row = random.choice(list(reader))
+            address = '|'.join(chosen_row)
+            return address
 
     def randomTime(self):
         # generate random number scaled to number of seconds in a day
@@ -48,42 +44,27 @@ class BankDetails:
         age = today.year - date_of_birth.year
         return age
 
-    def getPincode(self, city_name):
-        pincode_dict = {"Pune": [411057, 411024, 411000, 411003], "Mumbai": [400004, 400011, 400014, 400020],
-                        "Bangalore": [560009, 560015, 530068, 560002], "Calcutta": [700008, 700016, 700001,700012],
-                        "Delhi": [110095, 110092, 110053, 110048], "Jaipur": [302016, 302006, 302009, 302010],
-                        "Ahmedabad": [320008, 380004, 380011, 380022], "Rajkot": [360022, 360003, 360004, 360009]}
-        num = random.randint(0,3)
-        return pincode_dict[city_name][num]
-
 counter = 0
-while counter <= 100:
+while counter <= 1000000:
     bankdetails = BankDetails()
     customer_id = counter
     account_no = bankdetails.accountNo()
     acc_create_date = bankdetails.random_date('01/01/2000', '31/12/2022')
-    city_name = bankdetails.getCity()
-    state_name = bankdetails.getState(city_name)
+    address = bankdetails.getAdressdetails()
     created_time = bankdetails.randomTime()
     acc_updated_date = bankdetails.random_date('01/01/2000', '31/12/2022')
     updated_time = bankdetails.randomTime()
     date_of_birth = bankdetails.random_date('01/01/1965', '31/12/2002')
     age = bankdetails.getAge(date_of_birth)
-    pincode = bankdetails.getPincode(city_name)
 
     bank_details_list = [str(customer_id),str(account_no),acc_create_date.strftime("%m/%d/%Y") + ' ' + created_time,
-                         city_name,state_name,acc_updated_date.strftime("%m/%d/%Y") + ' ' + updated_time,date_of_birth.strftime("%m/%d/%Y"),str(age),str(pincode)]
+                             address,acc_updated_date.strftime("%m/%d/%Y") + ' ' + updated_time,
+                             date_of_birth.strftime("%m/%d/%Y"),str(age)]
 
     final_bank_details = '|'.join(bank_details_list)
-    print(final_bank_details)
+    # print(final_bank_details)
 
     with open('C:/Users/HP/Practice Assignments/bankdetails.csv', 'a') as file:
         file.write(final_bank_details + '\n')
-        file.close()
-
+    file.close()
     counter += 1
-
-
-
-
-# 1 Jan 2023
